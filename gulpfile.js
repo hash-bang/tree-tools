f36c2b07ba1722bafc65af54b61d3afae58af3fe
Matt Carter <m@ttcarter.com>
2017-11-16 15:30:54 +1000

    BUGFIX: Added full build process (uglify downstream needs Babel pre-step)

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
+var babel = require('gulp-babel');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var inject = require('gulp-inject-string');
+var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', ['build']);
 
-gulp.task('build', function () {
+gulp.task('build', ()=>
 	gulp.src('./index.js')
+		.pipe(plumber({
+			errorHandler: function(err) {
+				gutil.log(gutil.colors.red('ERROR DURING JS BUILD'));
+				process.stdout.write(err.stack);
+				this.emit('end');
+			},
+		}))
 		.pipe(rename('ngTreeTools.js'))
 		.pipe(inject.wrap('angular.module(\'ngTreeTools\', []).service(\'TreeTools\', function() {\n', '});'))
 		.pipe(replace(/^.*require\(.*\);\s+$/gm, ''))
 		.pipe(replace(/^var treeTools = .+$/m, 'return {'))
-		.pipe(gulp.dest('./dist'));
-});
+		.pipe(babel({
+			presets: ['es2015'],
+			plugins: ['angularjs-annotate'],
+		}))
+		.pipe(gulp.dest('./dist'))
+		.pipe(uglify())
+		.pipe(rename('ngTreeTools.min.js'))
+		.pipe(gulp.dest('./dist'))
+);
